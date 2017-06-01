4511ec1d998c04cd10dfa77055a1b128db9bee62
Freddy <dyks1300@student.nhl.nl>
2017-06-01 11:50:58 +0200

    API Test code in Application

--- Application/Application/Application/SelectieActivity.cs
+++ Application/Application/Application/SelectieActivity.cs
@@ -1,14 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 
 namespace Application
 {
@@ -20,11 +29,14 @@ namespace Application
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Selectie);
             // Create your application here
+            Getrelaties();
             var Klant = FindViewById<Button>(Resource.Id.Klant);
             Klant.Click += Klant_Click;
 
             var Leverancier = FindViewById<Button>(Resource.Id.Leverancier);
             Leverancier.Click += Leverancier_Click;
+
+            
         }
         private void Klant_Click(object sender, System.EventArgs e)
         {
@@ -34,7 +46,40 @@ namespace Application
         private void Leverancier_Click(object sender, System.EventArgs e)
         {
             StartActivity(typeof(LeverancierActivity));
+        }
+
+        public static async Task<string> Getrelaties()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            string result = "";
+            await MainActivity.Token();
+            var Authkey = "Bearer " + MainActivity.Authkey;
+            var APIkey = MainActivity.APIkey;
+
+            Console.WriteLine(Authkey);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", APIkey);
+            client.DefaultRequestHeaders.Add("Authorization", Authkey);
+
+            var uri = "https://b2bapi.snelstart.nl/v1/relaties" + queryString;
+
+            using (var response = await client.GetAsync(uri))
+            {
+                using (HttpContent content = response.Content)
+                {
+                    Console.WriteLine(response);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        result = await content.ReadAsStringAsync();
+                        System.IO.File.WriteAllText(@"C:\Users\Freddy\Desktop\Minor Systeemontwikkeling\Response\Getresponse.txt", Convert.ToString(result));
+                    }
+                    return result;
+                }
+            }
+        }
     }
-   }
-  }
+}
+
 
